1af040db4d91c9a26b2fccca1445ef6f61edd00b
ohjiyeon3275 <ojy940716@gmail.com>
2022-01-21 23:26:09 +0900

    add funcion for getting appended data

--- main.py
+++ main.py
@@ -12,6 +12,14 @@ def append(value):
         jsonfile.close()
     return "append success!"
 
+@app.route("/value")
+def read_value():
+    with open('append.json', mode='r', encoding='utf-8') as jsonfile:
+        data = json.load(jsonfile)
+        value = data["value"]
+        jsonfile.close()
+    return "value is " + value
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='3333')
